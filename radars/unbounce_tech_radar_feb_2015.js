255b15391c0fb28282b794e2542e470b849acbd7
David Dossot <david@dossot.net>
2015-06-18 12:47:51 -0700

    added missing semi-column

--- radars/unbounce_tech_radar_feb_2015.js
+++ radars/unbounce_tech_radar_feb_2015.js
@@ -2,7 +2,7 @@ var radar_title = "February 2015";
 var radar_sub_title = "technology radar";
 var radar_page_title = "Unbounce Technology Radar | " + radar_title;
 var radar_authors = [ "David Dossot", "Carl Schmidt", "Martin Suchanek", "Mike Thorpe" ];
-var previous_radar
+var previous_radar;
 
 var radar_arcs = [
                   {'r':100,'name':'Using','f':'#fff'},
